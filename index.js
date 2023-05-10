638fddbd066a885f54ac8b12077243f809398cd4
Heorhii Honcharov <0019@ukr.net>
2023-05-10 15:18:04 +0200

    firstlast

--- index.js
+++ index.js
@@ -1,19 +1,26 @@
 //--- Scroll Up Button Object
 const btnUp = {
-  element: document.querySelector('.btn-up'),
-  show() {this.element.hidden = false},
-  hide() {this.element.hidden = true},
+  element: document.querySelector(".btn-up"),
+  show() {
+    this.element.hidden = false;
+  },
+  hide() {
+    this.element.hidden = true;
+  },
   addEventListener() {
-    window.addEventListener('scroll', () => {
+    window.addEventListener("scroll", () => {
       const scrollY = window.scrollY || document.documentElement.scrollTop;
       scrollY > 500 ? this.show() : this.hide();
     });
-    document.querySelector('.btn-up').onclick = () => {
+    document.querySelector(".btn-up").onclick = () => {
       window.scrollTo({
         top: 0,
         left: 0,
-        behavior: 'smooth',
+        behavior: "smooth",
       });
     };
   },
 };
+
+// activation
+btnUp.addEventListener();
